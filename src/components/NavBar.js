e616c434601b4b31967a191e7b025ea1ea94db84
Carl <carlelli5@hotmail.co.uk>
2023-06-13 19:35:01 +0000

    added basic css styles to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,27 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/life-sort-logo-circle-logo.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
     return (
-    <Navbar expand="md">
-        <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="md" className={ styles.NavBar }>
+            <Navbar.Brand >
+                <img
+                src={ logo }
+                alt='logo'
+                height='50'
+                />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-                <Nav.Link href="#home">Login</Nav.Link>
-                <Nav.Link href="#link">Sign up</Nav.Link>
+            <Nav className="ms-auto text-start">
+                <Nav.Link >Dashboard</Nav.Link>
+                <Nav.Link >Login</Nav.Link>
+                <Nav.Link >Sign up</Nav.Link>
             </Nav>
             </Navbar.Collapse>
-        </Container>
     </Navbar>
     )
 };
