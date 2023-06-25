9502a4aa22491d355d1fb26ede193da0f38a1551
Carl <carlelli5@hotmail.co.uk>
2023-06-25 11:04:05 +0000

    added detailed holiday task which works with holiday task page to show the detail in a holiday task

--- src/pages/holiday-tasks/DetailedHolidayTask.js
+++ src/pages/holiday-tasks/DetailedHolidayTask.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { axiosRequest } from '../../api/axiosDefaults';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Col from 'react-bootstrap/Col';
+import styles from '../../styles/TaskDetail.module.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+import Button from 'react-bootstrap/Button';
+import btnStyles from '../../styles/Button.module.css'
 
-const DetailedHolidayTask = () => {
+
+const DetailedHolidayTask = (props) => {
 
     const history = useHistory();
 
@@ -10,11 +19,87 @@ const DetailedHolidayTask = () => {
         date_of_holiday,
         completed_state,
         title,
-        
+        description,
+        budget,
+        clothes,
+        passport,
+        holiday_insurance,
+        suitcases_packed,
+        holiday_paid_in_full,
+        car_hire,
+        tickets,
+        entertainment,
+        is_overdue,
+    } = props;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRequest.delete(`/holiday/${id}/`);
+            handleClose();
+            history.goBack();
+        } catch (error) {
+            console.log(error)
+        }
     }
 
+    const handleEdit = () => {
+        history.push(`/holiday/${id}/edit`)
+    }
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div>DetailedHolidayTask</div>
+        <Col className={`${styles.TaskCol}`}>
+            <div className='text-center'>
+                <Link to={`/holiday/${id}`} className={styles.TaskTitleLink}>
+                <h2 className={styles.TaskTitle}>{ title }</h2>
+                </Link>
+                <p>{ description }</p>
+                <p>Date of holiday: { date_of_holiday }</p>
+                <p>Cost of holiday: { budget }</p>
+                <p>{ passport ? 'Passport is okay' : 'Need to get passport' }</p>
+                <p>{ holiday_insurance ? 'Holiday insurance is okay' : 'Still need holiday insurance' }</p>
+                <p>{ suitcases_packed ? 'Suitcases are packed' : 'Still need to pack suitcases' }</p>
+                <p>{ car_hire ? 'Car hire is okay' : 'Still need car hire' }</p>
+                <p>{ tickets ? 'Tickets are okay' : 'Still need tickets' }</p>
+                <p>{ entertainment ? 'Entertainment is required' : 'No entertainment required'}</p>
+                <p>{ clothes ? 'Clothes all bought' : 'Still need to buy clothes' }</p>
+                <p>{ holiday_paid_in_full ? 'Holiday is all paid for' : 'Still need to finish paying for holiday' }</p>
+                <p>Current status for task: { completed_state }</p>
+                <p>{ is_overdue ? <span className={styles.Overdue}>Your holiday plan is overdue!</span> : <span className={styles.OnTime}>You are on schedule for your task!</span> }</p> 
+                <Button
+                className={btnStyles.ButtonStyle}
+                onClick={handleEdit}
+                >
+                    Edit
+                </Button>
+                <Button
+                className={btnStyles.ButtonStyle}
+                onClick={handleShow}
+                >
+                    Delete
+                </Button>
+            </div>
+            
+        <>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>Are you sure you want to delete this task?</Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                No go back!
+            </Button>
+            <Button variant="primary" onClick={handleDelete}>
+                Delete Task
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
+        </Col>
     )
 }
 
